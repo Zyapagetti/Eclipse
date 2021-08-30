7d60da67358ee3ed0c3c4a758ce02aef81faa7df
Zyapagetti <jeff9435@gmail.com>
2021-08-30 12:08:11 +0900

    #4.x

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-const bgSelector = document.querySelector("#change-background");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onWindowWidthChange() {
-  const WINDOW_WIDTH = window.innerWidth;
-  if (WINDOW_WIDTH < 300) {
-    bgSelector.classList.add("first");
-    bgSelector.classList.remove("second");
-  } else if (WINDOW_WIDTH >= 300 && WINDOW_WIDTH < 400) {
-    bgSelector.classList.remove("first");
-    bgSelector.classList.add("second");
-    bgSelector.classList.remove("third");
-  } else if (WINDOW_WIDTH >= 400 && WINDOW_WIDTH < 500) {
-    bgSelector.classList.remove("second");
-    bgSelector.classList.add("third");
-    bgSelector.classList.remove("fourth");
-  } else if (WINDOW_WIDTH >= 500 && WINDOW_WIDTH < 600) {
-    bgSelector.classList.remove("third");
-    bgSelector.classList.add("fourth");
-    bgSelector.classList.remove("fifth");
-  } else if (WINDOW_WIDTH >= 600) {
-    bgSelector.classList.remove("fourth");
-    bgSelector.classList.add("fifth");
-  }
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-onWindowWidthChange();
-window.addEventListener("resize", onWindowWidthChange);
+loginForm.addEventListener("submit", onLoginSubmit);
